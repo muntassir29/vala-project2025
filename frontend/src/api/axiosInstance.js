0e8a05693662d453d9daf1058356f0c56e171ed9
montassir <montassirchehboun@gmail.com>
2025-07-10 15:16:45 +0200

    update api and env frontend

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,11 +1,23 @@
-// src/api/axiosInstance.js
+// // src/api/axiosInstance.js
+// import axios from "axios";
+
+// const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api";
+
+// const axiosInstance = axios.create({
+//     baseURL: BASE_URL,
+//     withCredentials: true,
+// });
+
+// export default axiosInstance;
+
+
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api";
+const BASE_URL = import.meta.env.VITE_API_URL || "/api";
 
 const axiosInstance = axios.create({
-    baseURL: BASE_URL,
-    withCredentials: true,
+  baseURL: BASE_URL,
+  withCredentials: true,
 });
 
 export default axiosInstance;
