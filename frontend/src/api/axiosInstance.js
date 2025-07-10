9c088e9d0df145ef0d32c442ca5e3d83f3ca14d2
montassir <montassirchehboun@gmail.com>
2025-07-10 16:52:10 +0200

    update axios and tradecard

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -10,16 +10,42 @@
 
 // export default axiosInstance;
 
-// Forcing redeploy on Render
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// import axios from "axios";
+
+// const BASE_URL = import.meta.env.VITE_API_URL || "/api";
+
+// const axiosInstance = axios.create({
+//   baseURL: BASE_URL,
+//   withCredentials: true,
+// });
+
+// export default axiosInstance;
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// src/services/axiosInstance.js
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL || "/api";
+const BASE_URL = import.meta.env.VITE_API_URL || "https://trading-journal-am1b.onrender.com/api";
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
-  withCredentials: true,
 });
+
+// Ajout automatique du token à chaque requête
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default axiosInstance;
 
