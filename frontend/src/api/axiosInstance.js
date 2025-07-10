69f347caa448e14eec69cb090920fc0af201f34d
montassir <montassirchehboun@gmail.com>
2025-07-10 03:20:21 +0200

    update axios Instance

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,22 @@
+// // src/api/axiosInstance.js
+// import axios from "axios";
+
+// const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api";
+
+// const axiosInstance = axios.create({
+//     baseURL: BASE_URL,
+//     withCredentials: true,
+// });
+
+// export default axiosInstance;
+
+
 // src/api/axiosInstance.js
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api";
+const BASE_URL = import.meta.env.MODE === "development" 
+  ? "http://localhost:9000/api"  // attention ici le port est 9000 (d'après ton log render)
+  : "https://journaltrading-mr0j.onrender.com/api"; // URL backend render
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
