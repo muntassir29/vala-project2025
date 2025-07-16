00cba4fd525e7e71a00715b7ee97beb64a128468
montassir <montassirchehboun@gmail.com>
2025-07-16 04:51:17 +0200

    update total

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,30 +1,4 @@
-// // src/api/axiosInstance.js
-// import axios from "axios";
 
-// const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api";
-
-// const axiosInstance = axios.create({
-//     baseURL: BASE_URL,
-//     withCredentials: true,
-// });
-
-// export default axiosInstance;
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// import axios from "axios";
-
-// const BASE_URL = import.meta.env.VITE_API_URL || "/api";
-
-// const axiosInstance = axios.create({
-//   baseURL: BASE_URL,
-//   withCredentials: true,
-// });
-
-// export default axiosInstance;
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // src/services/axiosInstance.js
 import axios from "axios";
